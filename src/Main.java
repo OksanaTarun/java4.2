9628d90b141a8c3bf93e722e725d4bcafd3d9875
OksanaTarun <76516037+OksanaTarun@users.noreply.github.com>
2021-01-27 12:16:29 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double BMI = service.calculate(84.5, 1.86);
-        System.out.println(BMI);
+        double bmi = service.calculate(84.5, 1.86);
+        System.out.println(bmi);
 
     }
 }
